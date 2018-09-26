09a5b1be48f076e3eaf34579d78f2323c02b6000
Alessia Yi <alessia.yi@utec.edu.pe>
2018-09-26 17:10:16 -0500

    Avance delete

--- tree.h
+++ tree.h
@@ -39,41 +39,69 @@ class tree{
   }
 
   void constructTree(T num){
+    //izquierda, menor
+    //derecha, mayor
     if (!root){
       current=root=new Node<T>(num);
+      return;
     }
-    else{
-      Node<T>* to = new Node<T>(num);
-      if (current -> data==num){
-        throw "Ya existe";
-      }
-      if (current -> data > num){
-        if (current -> left){
-          current=current->left;
-          constructTree(num);
-        }
-        else{
-          current -> left=to;
-          current=root;
-        }
+    Node<T>* to = new Node<T>(num);
+    if (current -> data==num){
+      throw "Ya existe";
+    }
+    else if (current -> data > num){
+      if (current -> left){
+        current=current->left;
+        constructTree(num);
       }
       else{
-        if (current -> right){
-          current=current -> right;
-          constructTree(num);
-        }
-        else{
-          current -> right=to;
-          current=root;
-        }
+        current -> left=to;
+        current=root;
+      }
+    }
+    else{
+      if (current -> right){
+        current=current -> right;
+        constructTree(num);
+      }
+      else{
+        current -> right=to;
+        current=root;
       }
     }
   }
 
+  void swap(Node<T>* num, Node<T>* del){
+
+  }
+
   void removeTree(T num){
-    //0 hijos
-    //1 hijos
-    //2 hijos
+    //0 hijos, se elimina
+    //1 hijos, se cambia con el hijo y se elimina
+    //2 hijos, se encuentra el siguiente mayor, se cambia y se elimina
+    if (current -> data==num){
+      if (!num -> left && !num -> right){
+
+      }
+      else if (num -> left && !num -> right){
+
+      }
+      else if (!num -> left && num -> right){
+
+      }
+      else{
+
+      }
+      return;
+    }
+    else if (current -> data > num){
+      current=current->left;
+      removeTree(num);
+    }
+    else{
+      current=current -> right;
+      removeTree(num);
+    }
   }
 
   void printInorder(Node<T>* start){
