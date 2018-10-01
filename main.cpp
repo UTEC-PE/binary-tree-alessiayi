cae021d2d9485cac95b02a24cdf9e3057122a28b
Alessia Yi <alessia.yi@utec.edu.pe>
2018-09-30 20:37:49 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   tree<int> Tree;
 
-  Tree.insert(10);
+  Tree.insert(9);
+  Tree.insert(20);
+  Tree.insert(19);
   Tree.insert(7);
   Tree.insert(2);
-  Tree.insert(12);
+  Tree.insert(28);
+  Tree.insert(8);
   Tree.insert(1);
-  Tree.insert(14);
-  Tree.insert(11);
   Tree.insert(13);
 
   Tree.printTree();
 
   cout << "Peso: " << Tree.weight() << endl;
-
-  Tree.remove(7);
 
   cout << "Inorder: ";
   Tree.printInorderReal();
@@ -27,14 +26,28 @@ int main(int argc, char const *argv[]) {
   Tree.printPosorderReal();
   cout << "Preorder: ";
   Tree.printPreorderReal();
+  cout << endl;
+
+  Tree.remove(20);
+  Tree.remove(7);
 
   Tree.printTree();
 
   cout << "Peso: " << Tree.weight() << endl;
 
-  Iterator<int> It=new Iterator<int>(Tree.begin());
-  for (int i=0; i<Tree.weight(); i++){
-    ++It;
+  cout << "Inorder: ";
+  Tree.printInorderReal();
+  cout << "Posorder: ";
+  Tree.printPosorderReal();
+  cout << "Preorder: ";
+  Tree.printPreorderReal();
+  cout << endl;
+
+  Iterator<int> It=Tree.begin();
+
+  cout << *It << " ";
+  for (int i=0; i<Tree.weight()-1; i++){
+    cout << *++It << " ";
   }
 
   return 0;
